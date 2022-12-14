5e90f05358a9d0df902872c7a777b98040395608
Kami <qewadsyt@gmail.com>
2022-12-14 19:43:51 +0800

    feat: :sparkles: A file to create user info

---
+++ db/createUser.js
@@ -0,0 +1,9 @@
+const db = require("./database.js");
+/* db.inputUserData({
+	"userid": "551172679231602689", 
+	"user": "laor", 
+	"mcname": "archage", 
+	"mcuuid": "1296dadf-41c0-4a1c-8bc9-d89c33ee5040",
+	"interval": "2500",
+	"timestamp": new Date().getTime()
+}) */
