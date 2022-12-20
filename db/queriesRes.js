afd0c0333fcdbf4c53afd75cdc905aba763961af
Kami <harshimymommy@gmail.com>
2022-12-21 02:20:45 +0800

    chore: :fire: Remove empty console log

--- db/queriesRes.js
+++ db/queriesRes.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
 const TOML = require("@ltd/j-toml");
 const path = require("path");
-console.log();
 module.exports = TOML.parse(fs.readFileSync(`${__dirname}/queries.toml`, "utf8"), { joiner: "\n"})
