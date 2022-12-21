cb3379e78c24326f31366fbe5fad3c183b0c183c
Kami <harshimymommy@gmail.com>
2022-12-22 07:34:10 +0800

    perf: :zap: Compacted the file
    Remove trailing white spaces and unused functions to reduce load time!

--- db/queriesRes.js
+++ db/queriesRes.js
@@ -1,5 +1,1 @@
-const fs = require("fs");
-const TOML = require("@ltd/j-toml");
-const path = require("path");
-console.log();
-module.exports = TOML.parse(fs.readFileSync(`${__dirname}/queries.toml`, "utf8"), { joiner: "\n"})
+const fs = require("fs");const TOML = require("@ltd/j-toml");module.exports = TOML.parse(fs.readFileSync(`${__dirname}/queries.toml`, "utf8"), { joiner: "\n"})
