d28ff89b95fded96337165a7b46491f8d91c5bdc
Kami <harshimymommy@gmail.com>
2022-12-23 05:32:12 +0800

    feat(Tests): ✨ Pools multiple fetch requests
    Gets uuid from db then fetches it!
    
    No bypass to api key throttle..

--- fetch.js
+++ fetch.js
@@ -3,42 +3,50 @@ const db = require("./db/database.js");
 const fetch = require("node-fetch");
 require("dotenv").config();
 let apiUses = 0;
-let key = 0;
+let currentApiKey = 0;
 const users = db.allUserDataRes();
-const api = db.allApikeysRes();
+const api = db.apikeysRes()
 const uuid = [];
 users.forEach(index => {
 	uuid.push(index.mcuuid)
 })
 
-users.forEach((counter, index) => {
-	console.log(users[index].interval);
-	setInterval( async () => {
-		// TODO: Make statusRes into its own function to be ran after key throttle
-		let statusRes = await fetch(
-			`https://api.hypixel.net/status?key=${api[key].key}&uuid=${uuid[index]}`
-			);
-			apiUses++;
-			let status = await statusRes.json(); // TODO: Combine .json() to statusRes function
+const uuidRes = () => {
+	const fetchUuid = [];
+	uuid.forEach(eachUuid => { //TODO: ADD api key checks
+		fetchUuid.push(fetch(`https://api.hypixel.net/status?key=${api[currentApiKey]}&uuid=${eachUuid}`));
+	})
+	return fetchUuid;
+}
 
-			if(apiUses > 139 || (status.success === false && status.cause === "Key throttle")) {
-				// FIXME: Proper guard clause, proper key checking, proper throttles
-				key++;
-				// Guard clause 1
-				if(key >= api.length) {key = 0;} // TODO: Add a check that checks if every key is throttled with its own inkling
-				apiUses = 0;
-				console.log("Key throttled!!");
-				// FIXME: Better if else statements
-				statusRes = await fetch(
-					`https://api.hypixel.net/status?key=${api[key].key}&uuid=${uuid[index]}`
-					);
-				status = await statusRes.json();
-				console.log({api, key});
-				// TODO: Sends a message every interval, make minimal interval of bot to be max 1 request per minute 60000ms
-			}
-			console.table(status);
-			console.log(`${api[key].key}: ${apiUses}`);
-			console.log(key);
-			
-	}, users[index].interval);
+const statusRes = async () => {
+	const uuid = uuidRes();
+
+	const [...allResponses] = await Promise.all(uuid);
+
+	const myAsyncLoopFunction = async (array) => {
+  	const promises = array.map(res => res.json());
+		const finishedPromise = await Promise.all(promises);
+  	console.log(`All async responses has been turned to json!`);
+  	return finishedPromise;
+	}
+
+	return await myAsyncLoopFunction(allResponses);
+};
+
+const checkOnline = async () => {
+	const statuses = await statusRes();
+	const listOfOffline = [];
+	const offline = statuses.find((item, index) => {
+		if (item.success === false || !item.success) {
+			listOfOffline.push(item);
+		}
+	})
+	console.log(statuses);
+}
+checkOnline()
+process.on('SIGINT', function() {
+	DbStmt.close();
+	console.log("Closing fetch file.");
+	process.exit(0);
 })
